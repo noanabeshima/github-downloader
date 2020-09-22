b2562c4bf303d48e83108a9fe46bb247f19c0502
sdtblck <46172032+sdtblck@users.noreply.github.com>
2020-09-22 20:30:53 +0200

    Change joblib to multiprocessing

--- download_repo_text.py
+++ download_repo_text.py
@@ -8,13 +8,16 @@ import traceback
 import time
 import shutil
 import csv
-from joblib import Parallel, delayed
-from multiprocessing import cpu_count
+from multiprocessing import cpu_count, Pool
 from tqdm import tqdm
 import argparse
 
+
 mime = magic.Magic(mime=True)
 
+def split_into_chunks(l, n):
+    n = max(1, n)
+    return [l[i:i + n] for i in range(0, len(l), n)]
 
 def is_digit(x):
     return x in "1234567890"
@@ -83,88 +86,91 @@ def get_content(f):
 def process_repo_list(repo_data, archive_name='github_data'):
     ar = lmd.Archive(archive_name)
 
-    for i, repo in enumerate(tqdm(repo_data)):
-        name, stars, lang = repo
-        meta = {'repo_name': name, 'stars': stars, 'repo_language': lang}
-        repodir = f'./.tmp/{name.split("/")[-1]}'
-        os.system(f'git clone --depth 1 --single-branch https://github.com/{name} {repodir}')
-        shutil.rmtree(f'{repodir}/.git', ignore_errors=True)
+    for i, repo in enumerate(repo_data):
+        try:
+            name, stars, lang = repo
+            meta = {'repo_name': name, 'stars': stars, 'repo_language': lang}
+            repodir = f'./.tmp/{name.split("/")[-1]}'
+            os.system(f'git clone --depth 1 --single-branch https://github.com/{name} {repodir}')
+            shutil.rmtree(f'{repodir}/.git', ignore_errors=True)
 
-        for curdir, dirs, files in os.walk(repodir):
-            bad_extensions = [
-                'app',
-                'bin',
-                'bmp',
-                'bz2',
-                'class',
-                'csv',
-                'dat',
-                'db',
-                'dll',
-                'dylib',
-                'egg',
-                'eot',
-                'exe',
-                'gif',
-                'gitignore',
-                'glif',
-                'gradle',
-                'gz',
-                'ico',
-                'jar',
-                'jpeg',
-                'jpg',
-                'lo',
-                'lock',
-                'log',
-                'mp3',
-                'mp4',
-                'nar',
-                'o',
-                'ogg',
-                'otf',
-                'p',
-                'pdf',
-                'png',
-                'pickle',
-                'pkl',
-                'pyc',
-                'pyd',
-                'pyo',
-                'rkt',
-                'so',
-                'ss',
-                'svg',
-                'tar',
-                'tsv',
-                'ttf',
-                'war',
-                'webm',
-                'woff',
-                'woff2',
-                'xz',
-                'zip',
-                'zst'
-            ]
+            for curdir, dirs, files in os.walk(repodir):
+                bad_extensions = [
+                    'app',
+                    'bin',
+                    'bmp',
+                    'bz2',
+                    'class',
+                    'csv',
+                    'dat',
+                    'db',
+                    'dll',
+                    'dylib',
+                    'egg',
+                    'eot',
+                    'exe',
+                    'gif',
+                    'gitignore',
+                    'glif',
+                    'gradle',
+                    'gz',
+                    'ico',
+                    'jar',
+                    'jpeg',
+                    'jpg',
+                    'lo',
+                    'lock',
+                    'log',
+                    'mp3',
+                    'mp4',
+                    'nar',
+                    'o',
+                    'ogg',
+                    'otf',
+                    'p',
+                    'pdf',
+                    'png',
+                    'pickle',
+                    'pkl',
+                    'pyc',
+                    'pyd',
+                    'pyo',
+                    'rkt',
+                    'so',
+                    'ss',
+                    'svg',
+                    'tar',
+                    'tsv',
+                    'ttf',
+                    'war',
+                    'webm',
+                    'woff',
+                    'woff2',
+                    'xz',
+                    'zip',
+                    'zst'
+                ]
 
-            files = [curdir + '/' + f for f in files if '.git' not in f and f[
-                0] is not '.' and 'LICENSE' not in f and 'node_modules' not in f and '.min.' not in f and f.split('.')[
-                         -1] not in bad_extensions]
+                files = [curdir + '/' + f for f in files if '.git' not in f and f[
+                    0] is not '.' and 'LICENSE' not in f and 'node_modules' not in f and '.min.' not in f and f.split('.')[
+                             -1] not in bad_extensions]
 
-            filenames = [f.split("/")[-1] for f in files]
-            extensions = [mime.from_file(f) for f in files]
-            text_outputs = list(map(get_content, files))
-            for i in range(len(files)):
-                text = text_outputs[i]
-                if text is not None:
-                    meta['file_name'] = filenames[i]
-                    meta['mime_type'] = extensions[i]
+                filenames = [f.split("/")[-1] for f in files]
+                extensions = [mime.from_file(f) for f in files]
+                text_outputs = list(map(get_content, files))
+                for i in range(len(files)):
+                    text = text_outputs[i]
+                    if text is not None:
+                        meta['file_name'] = filenames[i]
+                        meta['mime_type'] = extensions[i]
 
-                    ar.add_data(text, meta)
+                        ar.add_data(text, meta)
 
-        shutil.rmtree(repodir, ignore_errors=True)
-        if (i + 1) % 100 == 0:
-            ar.commit()
+            shutil.rmtree(repodir, ignore_errors=True)
+            if (i + 1) % 100 == 0:
+                ar.commit()
+        except:
+            traceback.print_exc()
 
     ar.commit()
 
@@ -177,6 +183,9 @@ def process_args():
                         type=int)
     parser.add_argument('--n_stars', help='filter repos with less than n_stars stars',
                         default=-1,
+                        type=int)
+    parser.add_argument('--chunk_size', help='size of chunks to feed into each thread',
+                        default=100,
                         type=int)
     return parser.parse_args()
 
@@ -205,19 +214,14 @@ if __name__ == '__main__':
         repo_data = filter_by_stars(repo_data, args.n_stars)
     repo_data.sort()
 
-    random.seed(42)
+    random.seed(420)
     random.shuffle(repo_data)
 
-    n_threads = cpu_count() if args.n_threads == 0 else args.n_threads
+    n_threads = cpu_count() * 3 if args.n_threads == 0 else args.n_threads
     assert n_threads != 0
 
     # do work
-    chunk_size = int(len(repo_data) / n_threads)
-    repo_chunks = [None for _ in range(n_threads)]
-    for i in range(n_threads - 1):
-        repo_chunks[i] = repo_data[i * chunk_size:(i + 1) * chunk_size]
-    repo_chunks[-1] = repo_data[(n_threads - 1) * chunk_size:]
-
-    Parallel(n_jobs=n_threads, prefer="threads")(
-        delayed(process_repo_list)(chunk) for chunk in repo_chunks
-    )
+    repo_chunks = split_into_chunks(repo_data, args.chunk_size)
+    pool = Pool(n_threads)
+    for _ in tqdm(pool.imap_unordered(process_repo_list, repo_chunks), total=len(repo_chunks)):
+        pass
