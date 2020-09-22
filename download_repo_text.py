3bedf637b51c9225560b6d7dbc701837cf972e9e
sdtblck <sdtblck@gmail.com>
2020-09-22 19:36:50 +0200

    add command line arguments add filter by stars option

--- download_repo_text.py
+++ download_repo_text.py
@@ -9,33 +9,30 @@ import time
 import shutil
 import csv
 from joblib import Parallel, delayed
-
+from multiprocessing import cpu_count
 from tqdm import tqdm
+import argparse
 
 mime = magic.Magic(mime=True)
 
 
-def id(x):
-    return x
-
-def nonzero(x):
-    return filter(id, x)
-
 def is_digit(x):
     return x in "1234567890"
+
 
 def keep(x):
     num_digits = len(list(filter(is_digit, x)))
     num_newlines = len(list(filter(lambda x: x == '\n', x)))
     if num_digits / len(x) > 0.8:
         return False
-    
+
     # avg line length
-    if len(x) / (num_newlines+.001) > 200:
+    if len(x) / (num_newlines + .001) > 200:
         return False
-    
+
     return True
-    
+
+
 def get_content(f):
     type = None
     try:
@@ -45,7 +42,7 @@ def get_content(f):
             return
         with open(f, 'rb') as fromfh:
             buf = fromfh.read()
-        
+
         buf = buf.decode('UTF-8')
         if not keep(buf):
             return
@@ -82,17 +79,17 @@ def get_content(f):
         time.sleep(0.1)
         return
 
+
 def process_repo_list(repo_data, archive_name='github_data'):
     ar = lmd.Archive(archive_name)
 
     for i, repo in enumerate(tqdm(repo_data)):
         name, stars, lang = repo
-        meta = {'repo_name':name, 'stars':stars, 'repo_language':lang}
+        meta = {'repo_name': name, 'stars': stars, 'repo_language': lang}
         repodir = f'./.tmp/{name.split("/")[-1]}'
         os.system(f'git clone --depth 1 --single-branch https://github.com/{name} {repodir}')
         shutil.rmtree(f'{repodir}/.git', ignore_errors=True)
 
-        
         for curdir, dirs, files in os.walk(repodir):
             bad_extensions = [
                 'bin',
@@ -132,7 +129,9 @@ def process_repo_list(repo_data, archive_name='github_data'):
                 'zst'
             ]
 
-            files = [curdir + '/' + f for f in files if '.git' not in f and f[0] is not '.' and 'LICENSE' not in f and 'node_modules' not in f and '.min.' not in f and f.split('.')[-1] not in bad_extensions]
+            files = [curdir + '/' + f for f in files if '.git' not in f and f[
+                0] is not '.' and 'LICENSE' not in f and 'node_modules' not in f and '.min.' not in f and f.split('.')[
+                         -1] not in bad_extensions]
 
             filenames = [f.split("/")[-1] for f in files]
             extensions = [mime.from_file(f) for f in files]
@@ -152,30 +151,55 @@ def process_repo_list(repo_data, archive_name='github_data'):
     ar.commit()
 
 
+def process_args():
+    parser = argparse.ArgumentParser(
+        description='CLI for github downloader - A tool for scraping repos as text from github')
+    parser.add_argument('--n_threads', help='number of threads for parallel processing, defaults to cpu_count',
+                        default=0,
+                        type=int)
+    parser.add_argument('--n_stars', help='filter repos with less than n_stars stars',
+                        default=-1,
+                        type=int)
+    return parser.parse_args()
+
+
+def filter_by_stars(repo_data, n_stars):
+    return [item for item in repo_data if int(item[1]) >= n_stars]
+
+
 if __name__ == '__main__':
+
+    args = process_args() # parse args
+
+    # make output dirs
     if '.tmp' not in os.listdir():
         os.makedirs('.tmp')
     if 'github_data' not in os.listdir():
         os.makedirs('github_data')
 
+    # read repo data to a tuple (reponame, n_stars, language)
     with open('github_repositories.csv', 'r') as f:
         csv_reader = csv.reader(f)
         repo_data = list(map(tuple, csv_reader))
 
+    # filter by number of stars
+    if args.n_stars != -1:
+        repo_data = filter_by_stars(repo_data, args.n_stars)
     repo_data.sort()
+
     random.seed(42)
     random.shuffle(repo_data)
 
-    n_threads = 15
+    n_threads = cpu_count() if args.n_threads == 0 else args.n_threads
     assert n_threads != 0
-    chunk_size = int(len(repo_data)/n_threads)
 
+    # do work
+    chunk_size = int(len(repo_data) / n_threads)
     repo_chunks = [None for _ in range(n_threads)]
-    for i in range(n_threads-1):
-        repo_chunks[i] = repo_data[i*chunk_size:(i+1)*chunk_size]
-    repo_chunks[-1] = repo_data[(n_threads-1)*chunk_size:]
-
+    for i in range(n_threads - 1):
+        repo_chunks[i] = repo_data[i * chunk_size:(i + 1) * chunk_size]
+    repo_chunks[-1] = repo_data[(n_threads - 1) * chunk_size:]
 
     Parallel(n_jobs=n_threads, prefer="threads")(
         delayed(process_repo_list)(chunk) for chunk in repo_chunks
-        )
+    )
